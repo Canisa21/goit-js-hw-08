41b625a77fde37b076ca1f5c89aead010da50750
Canisa21 <jablonska.paul@gmail.com>
2024-02-04 20:33:14 +0100

    updated

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const createGallery = galleryItems.map(
+  (galleryItem) =>
+    `<li>
+      <a class="gallery__item" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${galleryItem.preview}" 
+        alt="${galleryItem.description}"
+        data-source="${galleryItem.original}"
+        >
+        </img>
+        </a>
+     </li>`
+).join("");
+
+gallery.insertAdjacentHTML("beforeend", createGallery);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  /* options */ captionsData: "alt",
+  captionDelay: 250,
+});
+lightbox.on("show.simplelightbox");
+
 console.log(galleryItems);
+
